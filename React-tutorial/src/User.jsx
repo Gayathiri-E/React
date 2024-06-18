355f0330376af8e112d00f33f5450e0654c7368d
gayathiri <gayathirie.21cse@kongu.edu>
2024-06-18 09:44:35 +0530

    initial

---
+++ React-tutorial/src/User.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+//using props
+// function User(props) {
+//   return (
+//     <div>
+//        <h1> {props.name}</h1>
+//        <h2>{props.age}</h2>
+//        <h3>{props.phone}</h3>
+//     </div>
+//   );
+// }
+
+
+//D- structuring method
+function User({name,age,phone}){
+    return (
+        <div>
+            <h1>{name}</h1>
+            <h1>{age}</h1>
+            <h1>{phone}</h1>
+        </div>
+    );
+}
+
+export default User
